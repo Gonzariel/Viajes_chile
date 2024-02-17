40e7986920c7ce799a0db5d0fee32997451a633a
Gonzariel <62854538+Gonzariel@users.noreply.github.com>
2024-02-16 21:28:47 -0300

    Smooth Scroll

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$(function() {
+
+    //smooth scroll//
+
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+  
+          event.preventDefault();
+ 
+          var hash = this.hash;
+    
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 200, function(){
+    
+            window.location.hash = hash;
+          });
+        } // End if
+      });
+});
