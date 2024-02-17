344a6eb14f98037f29370b21cb177e8f38bfda53
Gonzariel <62854538+Gonzariel@users.noreply.github.com>
2024-02-16 23:32:58 -0300

    Funcionalidades jquery,modificacion de estilos y correcciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,38 @@
 $(function() {
+    var contacto = $('#contactoButton');
+    var mensaje = $('.alert-info');
+    var presentacion = $('.iconPresentacion');
 
+    //alert fade contacto//    
+    contacto.click(function(){
+        
+        mensaje.fadeIn('slow');
+        mensaje.fadeOut(2000);
+        
+    });
+    
+    //Navbar background con el scroll
+    var navBar = $('nav');
+    $(window).scroll (function () {
+
+        console.log( $(this).scrollTop() );
+        if ($(this).scrollTop() > 200 ) { 
+            $('#toTop').fadeIn(); 
+            navBar.removeClass('bg-light');
+            navBar.addClass('bg-info');
+        } else {
+        navBar.removeClass('bg-light');
+        navBar.removeClass('bg-info');
+             $('#toTop').fadeOut();
+    }
+    });
+    
+    //slideTiggle presentacion//
+    presentacion.click(function(){
+        $('.pPresentacion').slideToggle('slow');
+    });
+    
     //smooth scroll//
-
     $("a").on('click', function(event) {
 
         if (this.hash !== "") {
@@ -17,5 +48,10 @@ $(function() {
             window.location.hash = hash;
           });
         } // End if
-      });
+    });
+
+    //toltip//
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    
 });
